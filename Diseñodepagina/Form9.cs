09936e146f86bb34ffd1ee77e47cc678a4fcccf0
RodrigoLinkIP <104653543+RodrigoLinkIP@users.noreply.github.com>
2022-05-04 18:57:08 -0500

    Add files via upload

---
+++ Diseñodepagina/Form9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diseñodepagina
+{
+    public partial class Form9 : Form
+    {
+        public Form9()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
